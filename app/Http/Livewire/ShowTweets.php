4c08365e7547b32de7b8fa3f9ae3f672750b16ed
Flavio Alves <jose.flavio@jsl.com.br>
2022-03-12 15:26:13 -0300

    finished basic application with livewire

---
+++ app/Http/Livewire/ShowTweets.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use App\Models\Tweet;
+use Livewire\Component;
+use Livewire\WithPagination;
+
+class ShowTweets extends Component
+{
+    use WithPagination;
+
+    public $content = 'Yoga flame';
+
+    protected $rules = [
+        'content' => 'required|min:3|max:255'
+    ];
+
+    public function render()
+    {
+        $tweets = Tweet::with('user')->latest()->paginate(10);
+        
+        return view('livewire.show-tweets', compact('tweets'));
+    }
+
+    public function create()
+    {
+        $this->validate();
+
+        auth()->user()->tweets()->create([
+            'content' => $this->content
+        ]);
+
+        $this->content = '';
+    }
+
+    public function like($id)
+    {
+        $tweet = Tweet::find($id);
+
+        $tweet->likes()->create([
+            'user_id' => auth()->user()->id
+        ]);
+    }
+
+    public function unlike(Tweet $tweet)
+    {
+        $tweet->likes()->delete();
+    }
+}
---
+++ resources/views/livewire/show-tweets.blade.php
@@ -0,0 +1,40 @@
+<div>
+    Show Tweets {{ $content }}
+
+    <form method="POST" wire:submit.prevent="create">
+        <input type="text" name="content" id="content" wire:model="content">
+        @error('content')
+            {{ $message }}
+        @enderror
+        <button type="submit">Criar Tweet</button>
+    </form>
+
+    <hr>
+
+    @foreach ($tweets as $tweet)
+        <div class="flex">
+            <div class="w-1/8">
+                @if ($tweet->user->photo)
+                    <img src="{{ url("storage/{$tweet->user->photo}") }}" alt="{{ $tweet->user->name }}" class="rounded-full h-8 w-8">
+                @else
+                    <img src="{{ url('imgs/no-image.png') }}" alt="{{ $tweet->user->name }}" class="rounded-full h-8 w-8">
+                @endif
+                {{ $tweet->user->name }} - 
+            </div>
+            <div class="w-7/8">
+                {{ $tweet->content }} 
+                @if($tweet->likes->count())
+                    <a href="#" wire:click.prevent="unlike({{ $tweet }})">Descurtir</a>
+                @else
+                    <a href="#" wire:click.prevent="like({{ $tweet->id }})">Curtir</a>
+                @endif
+            </div>
+        </div>
+        <br>
+    @endforeach
+
+    <hr>
+    
+    {{ $tweets->links() }}
+    
+</div>
